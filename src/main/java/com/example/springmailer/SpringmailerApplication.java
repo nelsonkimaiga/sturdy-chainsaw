073cd69894a550152b6fd4f8028af24804a42666
nelsonkimaiga <nelsonkimaiga@aol.com>
2018-11-06 10:43:54 +0300

    App init

---
+++ src/main/java/com/example/springmailer/SpringmailerApplication.java
@@ -0,0 +1,12 @@
+package com.example.springmailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmailerApplication.class, args);
+	}
+}
